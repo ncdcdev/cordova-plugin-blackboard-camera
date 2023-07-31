a86fbadcd7ae06c42418b7413f566e13b29ab27f
karuru6225 <e2hjhi@gmail.com>
2023-07-31 16:01:23 +0900

    オリジナルのExifを引き継ぐように修正。Exifバージョンも追加

--- src/ios/ElectronicBlackBoardManager.swift
+++ src/ios/ElectronicBlackBoardManager.swift
@@ -27,7 +27,12 @@ final class ElectronicBlackBoardManager: ElectronicBlackBoardManagerProtocol {
         let url = URL(fileURLWithPath: path)
         try? image.write(to: url)
         let cgImg = CGImageSourceCreateWithURL(url as CFURL, nil)
-         
+        let orgMeta = CGImageSourceCopyPropertiesAtIndex(cgImg!, 0, nil) as! [String: Any]
+        // // デバッグ出力
+        // print("orgMeta: \(orgMeta)")
+        // // exifを出力
+        // print("exif: \(orgMeta[kCGImagePropertyExifDictionary as String]!)")
+        
         let now = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy:MM:dd HH:mm:ss"
@@ -40,6 +45,7 @@ final class ElectronicBlackBoardManager: ElectronicBlackBoardManagerProtocol {
         // ColorSpace	色空間情報	0xA001	Short	1	◎
         // PixelXDimension	実効画像幅	0xA002	Short/Long	1	◎
         // PixelYDimension	実効画像高さ	0xA003	Short/Long	1	◎
+
         // SubSecTime	DateTimeのサブセック	0x9290	Ascii	Any	△
         // SubSecTimeOriginal	DateTimeOriginalのサブセック	0x9291	Ascii	Any	△
         // SubSecTimeDigitized	DateTimeDigitizedのサブセック	0x9292	Ascii	Any	△
@@ -47,18 +53,39 @@ final class ElectronicBlackBoardManager: ElectronicBlackBoardManagerProtocol {
         // DateTimeOriginal	原画像データの生成日時	0x9003	Ascii	20	△
         // DateTimeDigitized	デジタルデータの作成日時	0x9004	Ascii	20	△
 
+        // tiffヘッダ？の情報も書き換える
+        var tiff = orgMeta[kCGImagePropertyTIFFDictionary as String] as! [String: Any]
+        tiff[kCGImagePropertyTIFFImageDescription as String] = imageDescription
+        tiff[kCGImagePropertyTIFFModel as String] = model
+        tiff[kCGImagePropertyTIFFSoftware as String] = software
+        // exifを書き換える
+        var exif = orgMeta[kCGImagePropertyExifDictionary as String] as! [String: Any]
+        exif[kCGImagePropertyExifDateTimeOriginal as String] = formattedDate
+        exif[kCGImagePropertyExifSubsecTimeOriginal as String] = millisec
+        exif[kCGImagePropertyExifVersion as String] = [2, 3, 0]
+
+        // メタ情報を作成
         let metaData = [
-            kCGImagePropertyTIFFDictionary: [
-                kCGImagePropertyTIFFImageDescription: imageDescription,
-                kCGImagePropertyTIFFModel: model,
-                kCGImagePropertyTIFFSoftware: software,
-            ],
-            kCGImagePropertyExifDictionary: [
-                kCGImagePropertyExifDateTimeOriginal: formattedDate,
-                kCGImagePropertyExifSubsecTimeOriginal: millisec,
-                kCGImagePropertyExifVersion: "0230",
-            ]
+            kCGImagePropertyTIFFDictionary: tiff,
+            kCGImagePropertyExifDictionary: exif
         ] as CFDictionary
+
+        // let metaData = [
+        //     kCGImagePropertyTIFFDictionary: [
+        //         kCGImagePropertyTIFFImageDescription: imageDescription,
+        //         kCGImagePropertyTIFFModel: model,
+        //         kCGImagePropertyTIFFSoftware: software,
+        //     ],
+        //     kCGImagePropertyExifDictionary: [
+        //         kCGImagePropertyExifDateTimeOriginal: formattedDate,
+        //         kCGImagePropertyExifSubsecTimeOriginal: millisec,
+        //         kCGImagePropertyExifVersion: "0230",
+        //     ]
+        // ] as CFDictionary
+
+        // 埋め込むメタデータを表示
+        print("metaData: \(metaData)")
+
         let dest: CGImageDestination? = CGImageDestinationCreateWithURL(url as CFURL, kUTTypeJPEG, 1, nil)
         CGImageDestinationAddImageFromSource(dest!, cgImg!, 0, metaData)
         CGImageDestinationFinalize(dest!)
