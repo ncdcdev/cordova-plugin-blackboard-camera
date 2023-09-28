94dadd3808ecb95f596fc9cb14ac76353c68e018
JangSungChul <jang@ncdc.co.jp>
2023-09-28 10:25:32 +0900

    photoInfoのnull対応

--- src/android/ElectronicBlackBoardManager.kt
+++ src/android/ElectronicBlackBoardManager.kt
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat
 
 object ElectronicBlackBoardManager {
 
-    fun createImageEmbeddedMetaData(path: String, photoInfo: PhotoInfo, imageDescription: String, model: String, software: String) {
+    fun createImageEmbeddedMetaData(path: String, photoInfo: PhotoInfo?, imageDescription: String, model: String, software: String) {
 
         val formatter = SimpleDateFormat("yyyy:MM:dd HH:mm:ss", Locale.US)
         val now = Date()
@@ -23,23 +23,9 @@ object ElectronicBlackBoardManager {
         exif.setAttribute(ExifInterface.TAG_DATETIME_ORIGINAL, formattedDate)
         exif.setAttribute(ExifInterface.TAG_SUBSEC_TIME_ORIGINAL, millisec.toString())
         exif.setAttribute(ExifInterface.TAG_FLASH, "1")
+        if (photoInfo != null) {
+            exif.setAttribute(ExifInterface.TAG_XMP, photoInfo.toXMP())
+        }
         exif.saveAttributes()
-
-        // xmpの書き込み
-//        val xmp = photoInfo.toXMP()
-//        val swiftXmp = SwiftXMP()
-//
-//        return when (val result = swiftXmp.embedXmp(path, xmp)) {
-//            is Result.Success -> {
-//                println("add xmp successfully")
-//                result.data
-//            }
-//            is Result.Failure -> {
-//                println("add xmp error : ${result.exception.message}")
-//                null
-//            }
-//        }
     }
 }
-
-
