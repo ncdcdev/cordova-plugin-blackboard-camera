33cc08388b73d862659001bbc4ecba93c5c4c201
JangSungChul <jang@ncdc.co.jp>
2023-12-19 14:33:04 +0900

    同期

--- hooks/move_exifInterface.js
+++ hooks/move_exifInterface.js
@@ -20,7 +20,7 @@ module.exports = function (context) {
 
     // コピー先ディレクトリが存在するか確認し、存在しない場合は作成する
     if (!fs.existsSync(targetFile)) {
-      fs.mkdirSync(targetFile, { recursive: true });
+      fs.mkdir(targetFile, { recursive: true });
     }
 
     fs.rename(sourceFile, targetFile, function (err) {
