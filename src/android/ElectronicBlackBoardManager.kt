374519c8e17ac430456dba1e70140d65b94fa3cc
puffer-dev <jang@puffer-inc.com>
2024-10-11 14:13:54 +0900

    本家のExifInterfaceに変更

--- src/android/ElectronicBlackBoardManager.kt
+++ src/android/ElectronicBlackBoardManager.kt
@@ -1,7 +1,7 @@
 package jp.co.taisei.construction.fieldmanagement.plugin
 
+import android.media.ExifInterface
 import java.util.*
-import androidx.exifinterface.media.ExifInterfaceFix
 import com.adobe.xmp.XMPConst
 import java.text.SimpleDateFormat
 
@@ -18,15 +18,15 @@ object ElectronicBlackBoardManager {
         val now = Date()
         val millisec = (now.time % 1000).toInt()
         val formattedDate = formatter.format(now)
-        val exif = ExifInterfaceFix(path)
-        exif.setAttribute(ExifInterfaceFix.TAG_IMAGE_DESCRIPTION, imageDescription)
-        exif.setAttribute(ExifInterfaceFix.TAG_MODEL, model)
-        exif.setAttribute(ExifInterfaceFix.TAG_SOFTWARE, software)
-        exif.setAttribute(ExifInterfaceFix.TAG_DATETIME_ORIGINAL, formattedDate)
-        exif.setAttribute(ExifInterfaceFix.TAG_SUBSEC_TIME_ORIGINAL, millisec.toString())
-        exif.setAttribute(ExifInterfaceFix.TAG_FLASH, "1")
+        val exif = ExifInterface(path)
+        exif.setAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION, imageDescription)
+        exif.setAttribute(ExifInterface.TAG_MODEL, model)
+        exif.setAttribute(ExifInterface.TAG_SOFTWARE, software)
+        exif.setAttribute(ExifInterface.TAG_DATETIME_ORIGINAL, formattedDate)
+        exif.setAttribute(ExifInterface.TAG_SUBSEC_TIME_ORIGINAL, millisec.toString())
+        exif.setAttribute(ExifInterface.TAG_FLASH, "1")
         if (photoInfo != null) {
-            exif.setAttribute(ExifInterfaceFix.TAG_XMP, photoInfo.toXMP())
+            exif.setAttribute(ExifInterface.TAG_XMP, photoInfo.toXMP())
         }
         exif.saveAttributes()
     }
